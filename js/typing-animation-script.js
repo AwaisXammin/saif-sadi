9844a52889981ba520af08e0c974faef9eba3afe
Awais Raza <awais.xammin@gmail.com>
2022-11-09 14:42:02 +0500

    Home and about page done

---
+++ js/typing-animation-script.js
@@ -0,0 +1,14 @@
+$(function () {
+  $(".typing-text").typed({
+    strings: [
+      "I code cool website",
+      "I develop mobile apps",
+      "I love wordpress ",
+    ],
+    typeSpeed: 70,
+    backSpeed: 20,
+    backDelay: 1500,
+    showCursor: true,
+    loop: true,
+  });
+});
